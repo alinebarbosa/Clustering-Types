daf4758a955f1d10489d883923127280070700dd
alinebarbosa <liny_barbosa@hotmail.com>
2020-12-02 00:20:37 -0300

    out of the box model

--- modeloPlaces.py
+++ modeloPlaces.py
@@ -12,16 +12,24 @@ Modelo para classificar os places da Nohs Somos em 6 labels:
     Shows
     Eventos
     Hotéis
+    Saúde e bem-estar
 """
 
 # Import packages
 import pandas as pd
 import json
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, cohen_kappa_score, \
+                            classification_report, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.cluster import KMeans
+import numpy as np
 
 """Import data"""
 # Filepath 
 path = '/home/aline/Documentos/NohsSomos/ClusteringTypes/'
-#metadata_path = '/home/aline/Documentos/NohsSomos/metadata_local.json'
 
 # Read the data and store data in DataFrame
 tabela = pd.read_csv(path + 'tabela_places.csv')
@@ -73,8 +81,85 @@ typesDummified = pd.concat([types, dummies], axis=1)
 typesDummified.to_csv(path + 'tabelaDummificada.csv')
 
 # Check numbers of each type
-sumTypes = typesDummified.iloc[:,3:4704].sum()
+sumTypes = typesDummified.iloc[:,5:4704].sum()
 
 # Chech numbers of each type without establishment (because it is the most
 # frequenty type)
-sumWithoutEstablishment = typesDummified.loc[typesDummified['establishment'] == 0].iloc[:,3:4704].sum()
+sumWithoutEstablishment = typesDummified.loc[typesDummified['establishment'] == 0].iloc[:,5:4704].sum()
+
+
+"""Manually labeled data"""
+# Read the manually labeled data
+tableDummies = pd.read_csv(path + 'tableDummies.csv')
+
+# Drop row with all columns as nan values
+tableDummies = tableDummies.dropna(how='all')
+
+# Create a new dataframe with only labeled data
+labeled = tableDummies[tableDummies.label.notnull()]
+
+"""Model - Out of the box - Labeled data"""
+# Split data into train and validation 75/25 %
+train_X, val_X, train_y, val_y = train_test_split(labeled.iloc[:,6:106], 
+                                                  labeled['label'],
+                                                  random_state = 3)
+
+# Random Forest
+rf = RandomForestClassifier(n_estimators=400)
+rf.fit(train_X, train_y)
+rf_predictions = rf.predict(val_X)
+accuracy_score(val_y, rf_predictions)
+confusion_matrix(val_y, rf_predictions)
+print(classification_report(val_y, rf_predictions))
+cohen_kappa_score(val_y, rf_predictions)
+
+#SVM 
+svc = SVC()
+svc.fit(train_X, train_y)
+svc_predictions = svc.predict(val_X)
+accuracy_score(val_y, svc_predictions)
+confusion_matrix(val_y, svc_predictions)
+print(classification_report(val_y, svc_predictions))
+cohen_kappa_score(val_y, svc_predictions)
+
+# KNN
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(train_X, train_y)
+knn_predictions = svc.predict(val_X)
+accuracy_score(val_y, knn_predictions)
+confusion_matrix(val_y, knn_predictions)
+print(classification_report(val_y, knn_predictions))
+cohen_kappa_score(val_y, knn_predictions)
+
+"""Model - Clustering data"""
+# Split data into train and validation 75/25 %
+trainComplete_X, valComplete_X, trainComplete_y, valComplete_y = \
+                                train_test_split(tableDummies.iloc[:,6:106], 
+                                                 tableDummies['label'],
+                                                 random_state = 3)
+#-------------------------------
+k=5
+kmeans = KMeans(n_clusters=k)
+X_dist = kmeans.fit_transform(trainComplete_X) 
+representative_idx = np.argmin(X_dist, axis=0)
+X_representative = trainComplete_X.values[representative_idx]
+
+y_representative = [list(trainComplete_y)[x] for x in representative_idx]
+#------------------------------
+
+# Modelo para usar dados com label para predizer todo o conjunto de dados e 
+# salvar csv
+
+#Fit all labeled data
+rf.fit(labeled.iloc[:,6:106], labeled['label'])
+rf_predictions = rf.predict(tableDummies.iloc[:,6:106])
+
+pd.DataFrame(rf_predictions).to_csv(path + 'outOfTheBoxPredictions.csv')
+
+
+
+#--------------------------------
+accuracy_score(tableDummies['label'], rf_predictions)
+confusion_matrix(valComplete_y, rf_predictions)
+print(classification_report(valComplete_y, rf_predictions))
+cohen_kappa_score(valComplete_y, rf_predictions)
